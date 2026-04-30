bb41f8900a57264ca9d040c16282ff15da05ccc3
itsLucas02 <aizzulluqman454@gmail.com>
2026-04-30 11:54:31 +0800

    feat: initial commit

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,32 @@
+import { Geist, Geist_Mono, Public_Sans, Merriweather } from "next/font/google"
+
+import "@workspace/ui/globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@workspace/ui/lib/utils";
+
+const merriweatherHeading = Merriweather({subsets:['latin'],variable:'--font-heading'});
+
+const publicSans = Public_Sans({subsets:['latin'],variable:'--font-sans'})
+
+const fontMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn("antialiased", fontMono.variable, "font-sans", publicSans.variable, merriweatherHeading.variable)}
+    >
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  )
+}
